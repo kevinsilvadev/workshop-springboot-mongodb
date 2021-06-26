6d949d888b9749866e1f4a2c80b3a65eeccdd8c9
Kevin-Silva-Dev <kivim2018@gmail.com>
2021-06-26 17:43:57 -0300

    Project created

---
+++ src/test/java/com/kevinsilva/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinsilva.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
