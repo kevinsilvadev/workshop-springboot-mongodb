3b0a043d95148768a6e6034d2f2da9a8e72282d1
Kevin-Silva-Dev <kivim2018@gmail.com>
2021-06-28 18:53:31 -0300

    Insercao de usuario com POST

--- src/main/java/com/kevinsilva/workshopmongo/controller/UserController.java
+++ src/main/java/com/kevinsilva/workshopmongo/controller/UserController.java
@@ -1,36 +1,49 @@
 package com.kevinsilva.workshopmongo.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kevinsilva.workshopmongo.domain.User;
 import com.kevinsilva.workshopmongo.dto.UserDTO;
 import com.kevinsilva.workshopmongo.services.UserService;
 
-
-@Controller
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 
-	@GetMapping(value = "/users")
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 
 	}
-	
-	@GetMapping(value = "/users/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User obj = service.FromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/kevinsilva/workshopmongo/controller/exception/StandardError.java
+++ src/main/java/com/kevinsilva/workshopmongo/controller/exception/StandardError.java
@@ -14,11 +14,11 @@ import lombok.Setter;
 public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-	
+
 }
