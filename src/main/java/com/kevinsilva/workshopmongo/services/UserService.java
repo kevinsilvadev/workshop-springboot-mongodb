db86b29cff86581b097e78075241f7f8e14c30c6
Kevin-Silva-Dev <kivim2018@gmail.com>
2021-06-28 19:43:47 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/kevinsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/kevinsilva/workshopmongo/services/UserService.java
@@ -25,12 +25,20 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+
 	
 	public User FromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+
 }
